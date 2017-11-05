bcd5b20e9e2aad184a9e8adb97e22431c5edabd5
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-11-06 00:02:32 +0600

    Updates

--- jarray.js
+++ jarray.js
@@ -4,163 +4,134 @@
  * Version: 1.0
  */
 
-window.jarray = (function() {
-	
-	return {
+(function(global) {
 
-		contain(arr, element) {
-			if (arr.includes(element)) {
-				return true;
-			}
-			return false
-		},
+  global.jar = (function() {
 
-		without(arr, withoutArr) {
-			var newArr = [];
-			for (var i = 0; i < arr.length; i++) {
-				if (!withoutArr.includes(arr[i])) {
-					newArr.push(arr[i]);
-				}
-			}
-			return newArr;
-		},
+    return {
 
-		every(arr, index) {
-			var newArr = [];
-			var indexUp = index;
-			for (var i = 0; i < arr.length; i++) {
-				if (i === indexUp) {
-					newArr.push(arr[i]);
-					indexUp += index;
-				}
-			}
-			return newArr;
-		},
+      contain(arr, element) {
+        return arr.includes(element) ? true : false;
+      },
 
-		randomRange(arr, a, b) {
-	   	var newArr = [];
-	    arr.forEach(i => {
-        if (i >= a && i <= b) {
-          newArr.push(i);
+      even(arr) {
+        var newArr = [];
+        arr.forEach(i => {
+          if ((typeof i === 'number') && (i % 2 === 0)) {
+            newArr.push(i);
+          }
+        });
+        return newArr;
+      },
+
+      every(arr, index) {
+        var newArr = []; 
+        var indexUp = index;
+        for (var i = 0; i < arr.length; i++) {
+          if (i === indexUp) {
+            newArr.push(arr[i]);
+            indexUp += index;
+          }
         }
-      });
-			return newArr[~~(Math.random() * newArr.length)];
-		},
+        return newArr;
+      },
 
-		odd(arr) {
-			var newArr = [];
-			arr.forEach(i => {
-				if ((typeof i === 'number') && (i % 2 !== 0)) {
-					newArr.push(i);
-				}
-			});
-  		return newArr;
-		},
+      randomRange(arr, a, b) {
+         var newArr = [];
+        arr.forEach(i => {
+          if (i >= a && i <= b) {
+            newArr.push(i);
+          }
+        });
+        return newArr[~~(Math.random() * newArr.length)];
+      },
 
+      odd(arr) {
+        var newArr = [];
+        arr.forEach(i => {
+          if ((typeof i === 'number') && (i % 2 !== 0)) {
+            newArr.push(i);
+          }
+        });
+        return newArr;
+      },
 
-		max(arr) {
-			var max = 0;
-			for (var i = 0; i <= arr.length; i++) {
-				if (typeof i === 'number') {
-					max = i > max ? i : 0;
-				}
-			}
-			return max;
-		},
+      max(arr) {
+        return Math.max.apply(null, arr);
+      },
 
-		first(arr) {
-			return arr[0];
-		},
-		/////
+      first(arr) {
+        return arr[0];
+      },
 
+      last(arr) {
+        return arr[arr.length - 1];
+      },
 
+      random(arr) {
+        return arr[~~(Math.random() * arr.length)];
+      },
 
-		last(arr) {
-			return arr[arr.length - 1];
-		},
+      shuffle(arr) {
+        return arr.sort(() => ~~(Math.random() * arr.length));
+      },
 
-		random(arr) {
-			return arr[~~(Math.random() * arr.length)];
-		},
+      unique(arr) {
+        return [...new Set(arr)];
+      },
 
-		shuffle(arr) {
-		  return arr.sort(() => ~~(Math.random() * arr.length));
-		},
+      reverse(arr) {
+        return arr.sort((a, b) => b - a);
+      },
 
-		unique(arr) {
-			var newArr = [];
-	    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] != newArr[newArr.indexOf(arr[i])]) {
-          newArr.push(arr[i]);
+      propsLength(arr) {
+        return arr.map(i => i.length);
+      },
+
+      sum(arr) {
+        return arr.reduce((a, b) => a + b);
+      },
+
+      sort(arr, sign) {
+        if (sign === '>') {
+          return arr.sort((a, b) => a > b);
+        } else if (sign === '<') {
+          return arr.sort((a, b) => a < b);
         }
-	    }
-  		return newArr;
-		},
+      },
 
-		reverse(arr) {
-			return arr.sort((a, b) => b - a);
-		},
+      sortRange(arr, a, b) {
+        return arr.map(i => {
+          if (i >= a && i <= b) i;
+        });
+      },
 
-		propsLength(arr) {
-			return arr.map(i => i.length);
-		},
+ 
 
-		sum(arr) {
-			return arr.reduce((a, b) => a + b);
-		},
-
-		sort(arr, sign) {
-			if (sign === '>') {
-				return arr.sort((a, b) => a > b);
-			} else if (sign === '<') {
-				return arr.sort((a, b) => a < b);
-			}
-		},
-
-		sortRange(arr, a, b) {
-			var newArr = [];
-	    arr.forEach(i => {
-        if (i >= a && i <= b) {
-          newArr.push(i);
+      missing(arr) {
+        var arrOfNums = [];
+        for (var i = 1; i <= Math.max.apply(null, arr); i++) {
+          arrOfNums.push(i);
         }
-      });
-  		return newArr;
-		},
+        var missing = [];
+        arr.sort((a, b) => a > b);
+        for (var i = 0; i < Math.max.apply(null, arr); i++) {
+          if (!arr.includes(arrOfNums[i])) {
+            missing.push(arrOfNums[i]);
+          }
+        }
+        if (missing.length === 0) {
+          return false;
+        }
+        return missing;
+      },
 
-		even(arr) {
-			var newArr = [];
-			arr.forEach(i => {
-				if ((typeof i === 'number') && (i % 2 === 0)) {
-					newArr.push(i);
-				}
-			});
-  		return newArr;
-		},
+      without(arr, withoutArr) {
+        return arr.map(i => !withoutArr.includes(i) ? i : '')
+      },
+    
+    }
 
-		/**
-		 * Find missing num from 1 to arr.length
-		 */
-		missing(arr) {
-			var arrOfNums = [];
-			for (var i = 1; i <= Math.max(...arr); i++) {
-				arrOfNums.push(i);
-			}
-			var missing = [];
-			arr.sort((a, b) => a > b);
-			for (var i = 0; i < Math.max(...arr); i++) {
-				if (!arr.includes(arrOfNums[i])) {
-					missing.push(arrOfNums[i]);
-				}
-			}
-			if (missing.length === 0) {
-				return false;
-			}
-			return missing;
-		}
-		/**
-		 * End of Array methods
-		 */
+  })();
 
-	}
-
-}());
+})(this);
